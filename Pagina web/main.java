2bb35e0881edbef28484234123f0edc27c8acc4b
Juan-Alejandro-Arriaga <juanalejandroarriaganava@gmail.com>
2025-02-25 09:28:08 -0600

    Update New Proofs

---
+++ Pagina web/main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+
+public class main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Si");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+        frame.getContentPane().setBackground(new java.awt.Color(200, 130, 150));
+    }
+}
+
+
+
+
+
