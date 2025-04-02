89dd8066b0572ddedc8141398a0887fd247da063
Juan-Alejandro-Arriaga <juanalejandroarriaganava@gmail.com>
2025-04-02 11:24:42 -0600

    Add initial implementations of funciones.java and function.js

---
+++ funciones.java
@@ -0,0 +1,12 @@
+public class funciones {
+
+    public static void main(String[] args) {
+        int salir = 0;
+        do {
+            System.out.println("no salir");
+        }while (salir == 1);
+
+    }
+
+}
+
---
+++ function.js
@@ -0,0 +1,6 @@
+let fin = 1;
+do {
+    fin++;
+    console.log("Hola Mundo " + fin);
+    
+}while(fin != 10);
